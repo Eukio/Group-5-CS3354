3f19e755ad548df78ed55bfd862e1dce9f11c59d
lobo017 <edaniel.lobo@gmail.com>
2025-04-01 22:40:22 -0500

    finished create_post logic and tests based on requirements

---
+++ src/Create_Post.js
@@ -0,0 +1,19 @@
+function Create_Post(header, description){
+    if (header.length > 0 && header.length <= 50 && description.length >= 25 && description.length <= 500){
+        return true;
+    }
+    if(header.length == 0){
+        return "Header cannot be empty";
+    }
+    if(header.length > 50){
+        return "Header cannot be more than 50 characters";
+    }
+    if(description.length == 0){
+        return "Description cannot be empty";
+    }
+    if(description.length > 500){
+        return "Description cannot be more than 500 characters";
+    }
+    return false;    
+}
+module.exports = Create_Post;
---
+++ src/Create_Post.test.js
@@ -0,0 +1,18 @@
+const Create_Post = require('./Create_Post.js');
+
+
+test('Valid Post', () => {
+    expect(Create_Post('Hello', 'This is a description. It is longer than 25 characters.')).toBe(true);
+});
+test('Empty Header', () => {
+    expect(Create_Post('', 'This is a description')).toBe('Header cannot be empty');
+});
+test('Empty Description', () => {
+    expect(Create_Post('Hello', '')).toBe('Description cannot be empty');
+});
+test('Header too long', () => {
+    expect(Create_Post('Hello this is a very long header that is more than 50 characters', 'This is a description')).toBe('Header cannot be more than 50 characters');
+});
+test('Description too long', () => {
+    expect(Create_Post('Hello', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')).toBe('Description cannot be more than 500 characters');
+});
