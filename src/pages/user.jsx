d9829c155f45a310b42c5bf0e4760ad8ffac550c
Geetanshi Jindal <geetanshi.jindal@outlook.com>
2025-04-24 02:49:23 -0500

    user.jsx

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import "../styles/user.css";
 
 function User() {
     const navigate = useNavigate();
@@ -7,30 +8,28 @@ function User() {
         navigate('/login');
     };
     return (
-        <>
-            <h1>User Page</h1>
-            <button onClick={handleClick1} className="posts"> Sign out </button>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-        </>
+        <div className="page">
+            <div className="profile-container">
+                <div className="avatar">
+                  < i class="fa-solid fa-user"></i>
+                </div>
+                <div class="profile">
+                   <p><strong>Name:</strong> April Smith</p>
+                   <p><strong>Email ID:</strong> yourcontact@utdallas.edu</p>
+                </div>
+            </div>
+            <div className="joined-clubs">
+                <p><Strong>Group Leader </strong></p>
+
+            </div>
+            <button onClick={handleClick1} className="sign-out"> Sign out </button>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        </div>
     );
 }
 
