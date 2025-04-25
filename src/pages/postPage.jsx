8b9e9b816ff93a92dcda494c49462f22edf4d913
Geetanshi Jindal <geetanshi.jindal@outlook.com>
2025-04-25 16:18:23 -0500

    post page: frontend finished

--- src/pages/postPage.jsx
+++ src/pages/postPage.jsx
@@ -4,6 +4,9 @@ import "../styles/postPage.css";
 import chess_post from '../assets/chess-post.png';
 import art_post from '../assets/art-post.png';
 import debate_post from '../assets/debate-post.png';
+import eco_post from '../assets/eco-post.png';
+import stem_post from '../assets/STEM-post.png';
+import women_post from '../assets/women-networking-post.png';
 
 function PostPage() {
     const navigate = useNavigate();
@@ -31,7 +34,25 @@ function PostPage() {
                 </div>
                 <div className="added-post">
                     <div className="image-wrapper">
-                        <img src={debate_post} alt="Post2" className="post-image" />
+                        <img src={debate_post} alt="Post3" className="post-image" />
+                    </div>
+                    <button onClick={handleClick1}> Explore </button>
+                </div>
+                <div className="added-post">
+                    <div className="image-wrapper">
+                        <img src={eco_post} alt="Post2" className="post-image" />
+                    </div>
+                    <button onClick={handleClick1}> Explore </button>
+                </div>
+                <div className="added-post">
+                    <div className="image-wrapper">
+                        <img src={stem_post} alt="Post2" className="post-image" />
+                    </div>
+                    <button onClick={handleClick1}> Explore </button>
+                    </div>
+                <div className="added-post">
+                    <div className="image-wrapper">
+                        <img src={women_post} alt="Post2" className="post-image" />
                     </div>
                     <button onClick={handleClick1}> Explore </button>
                 </div>
