c0f6e79997f784675309bc47eebd385b9ca4f974
Geetanshi Jindal <geetanshi.jindal@outlook.com>
2025-04-25 14:10:52 -0500

    updated postPage

--- src/pages/postPage.jsx
+++ src/pages/postPage.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-//import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import "../styles/postPage.css";
 function PostPage() {
+    const navigate = useNavigate();
+    const handleClick1 = () => {
+        navigate('/explore');
+    };
 
     return (
         <div className="page">
             <div className = "post-main-heading">
                 <h1> Popular Posts </h1>
             </div>
-            <div className="added-posts">
+            <div className="all-posts">
+                <div className="added-posts">
 
+                    <button onClick={handleClick1} className="explore-button"> Explore </button>
+                </div>
             </div>
 
         </div>
