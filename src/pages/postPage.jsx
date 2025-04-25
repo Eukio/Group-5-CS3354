cef846b015d1a795e656bd3c5dd1f90d86fd67bb
Geetanshi Jindal <geetanshi.jindal@outlook.com>
2025-04-25 15:22:31 -0500

    update post page

--- src/pages/postPage.jsx
+++ src/pages/postPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import "../styles/postPage.css";
+import chess_post from '../assets/art-post.png';
+
 function PostPage() {
     const navigate = useNavigate();
     const handleClick1 = () => {
@@ -13,9 +15,9 @@ function PostPage() {
                 <h1> Popular Posts </h1>
             </div>
             <div className="all-posts">
-                <div className="added-posts">
-
-                    <button onClick={handleClick1} className="explore-button"> Explore </button>
+                <div className="added-post">
+                    <img src={chess_post} alt="post1" className="post-image"/>
+                    <button onClick={handleClick1}> Explore </button>
                 </div>
             </div>
 
