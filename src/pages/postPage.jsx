fe56d0f823eb9c267b38698fc4a6eaabc5aacc49
Geetanshi Jindal <geetanshi.jindal@outlook.com>
2025-04-25 13:46:36 -0500

    updated postPage

--- src/pages/postPage.jsx
+++ src/pages/postPage.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 //import { useNavigate } from 'react-router-dom';
 import "../styles/postPage.css";
-import banner from '../assets/banner.png';
 function PostPage() {
 
     return (
         <div className="page">
             <div className = "post-main-heading">
-                <div className="main-img"><img src={banner} alt="Campus" /></div>
-                <div className="main-text"> Popular Posts </div>
+                <h1> Popular Posts </h1>
+            </div>
+            <div className="added-posts">
+
             </div>
 
         </div>
