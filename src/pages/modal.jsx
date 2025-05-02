3eb8939ab53fd70d497d1e2c673361e1a283f65b
evan <boltwu10@gmail.com>
2025-05-02 10:18:37 -0500

    changed club leader to the user ID that created it

--- src/pages/modal.jsx
+++ src/pages/modal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "../styles/modal.css"; // We will create this CSS file next
+import "../styles/modal.css"; 
 
 const Modal = ({ isOpen, onClose, club }) => {
   if (!isOpen) return null;
@@ -10,7 +10,7 @@ const Modal = ({ isOpen, onClose, club }) => {
         <h2>{club.name}</h2>
         <p>{club.description}</p>
         <p><strong>Members:</strong> {club.memberCount}</p>
-        <p><strong>Club Leader:</strong> {club.leader}</p>
+        <p><strong>User ID:</strong> {club.organizerId}</p>
         <button onClick={onClose}>Close</button>
       </div>
     </div>
